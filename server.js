28a24b63a898d17f550cd4fad2fa4e8605f8484a
yeti-detective <yeti.detective@gmail.com>
2018-08-21 20:33:08 -0400

    prettier

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
-app.get('/abi', (req, res) => {
-  res.json(JSON.parse(fs.readFileSync(path.join(__dirname, 'build', 'contracts', 'Casino.json'))).abi);
-})
+app.get("/abi", (req, res) => {
+  res.json(
+    JSON.parse(
+      fs.readFileSync(path.join(__dirname, "build", "contracts", "Casino.json"))
+    ).abi
+  );
+});
 
-app.get('/:resource', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', req.params.resource));
-})
+app.get("/:resource", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", req.params.resource));
+});
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
-})
+});
